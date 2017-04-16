66ff62847c7b9afeabc3397f81e6ad6e242774c8
619086871 <619086871@qq.com>
2017-04-17 02:42:21 +0800

    设计了 TableDataGetter 类 1. TableDataGetter 类用来从AutoCAD中的文字或者表格中提取出表格数据； 2. 实现了多次选择行或列来进行表格的输出； 3. 后面要继续实现一次性将整个表格进行输出；

---
+++ eZcad/Examples/Example_AddMenuItem.cs
@@ -0,0 +1,58 @@
+﻿using AutoCAD;
+using Autodesk.AutoCAD.ApplicationServices;
+
+namespace AutoCADDev
+{
+    public class Example_AddMenuItem
+    {
+        /// <summary> 添加菜单的操作可以直接在AutoCAD运行过程中执行，而不用像Revit中一样必须要先关闭然后在程序启动时添加 </summary>
+        private static void AddMenuItem()
+        {
+            // This example creates a new menu called TestMenu and inserts a menu item
+            // into it. The menu is then displayed on the menu bar.
+            // To remove the menu after execution of this macro, use the Customize Menu
+            // option from the Tools menu.
+
+            var app = Application.AcadApplication as AcadApplication;
+
+            // AcadMenuGroup 表示AutoCAD中加载的一个.cuix文件中的菜单集合。  
+            // MenuGroups.Item(0) 与 Menubar 中的菜单集合 并不是同一个集合，它的 MenuFileName 属性对应的文件为：
+            // C:\Users\zengfy\appdata\roaming\autodesk\autocad 2014\r19.1\chs\support\acad.cuix
+            AcadMenuGroup currMenuGroup = app.MenuGroups.Item(0);
+
+            //Create the new menu
+            AcadPopupMenu topPpMenu = currMenuGroup.Menus.Add("MenuHandler");
+
+            // Assign the macro string the VB equivalent of "ESC ESC _open "
+            string openMacro = "ESC ESC _open "; // VBA 中的写法：openMacro = Chr(3) & Chr(3) & Chr(95) & "open" & Chr(32)
+
+            //Add a menu item to the new menu, Macro 即对应要在命令行中输入的字符
+            AcadPopupMenuItem childMenuItem = topPpMenu.AddMenuItem(Index: topPpMenu.Count + 1, Label: "Open",
+                Macro: openMacro);
+
+            // 添加可以包含子项的菜单
+            AcadPopupMenu menuItemContainer = topPpMenu.AddSubMenu(Index: topPpMenu.Count + 1, Label: "block");
+            AcadPopupMenuItem childMenuItem2 = menuItemContainer.AddMenuItem(Index: menuItemContainer.Count + 1,
+                Label: "画圆", Macro: "Circle ");
+
+            // Display the menu on the menu bar
+
+            // If index is a string, the name of the popup menu must contain any underscore that appears in the menu. 
+            topPpMenu.InsertInMenuBar(Index: "工具(&T)");
+            return;
+            // 以下为功能测试项 --------------------------------------------------------
+
+            // The index must be either an integer or a string. If an integer, the index must be between 0 and N-1, where N is the number of objects in the menu bar. The new item will be added immediately before the specified index location. 
+            topPpMenu.InsertInMenuBar(Index: app.MenuBar.Count + 1);
+
+            // 从 MenuBar 提取一个菜单项。
+            // 注意：MenuBar 只能用来读取其中的 AcadPopupMenu 集合，而不能操纵菜单项的添加或移除。
+            AcadMenuBar mb = app.MenuBar;
+            // 下面这个Item(0)对应的是AutoCAD界面中菜单栏中的第一个菜单项，通常这个菜单项为“文件(F)”
+            AcadPopupMenu ppm1 = mb.Item(0);
+
+            // 将菜单项从 MenuBar 中移除
+            topPpMenu.RemoveFromMenuBar();
+        }
+    }
+}
--- eZcad/Examples/GraphicalElementsCreator.cs
+++ eZcad/Examples/GraphicalElementsCreator.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using AutoCADDev.Graphics;
+using AutoCADDev.Examples;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
@@ -11,7 +11,7 @@ using Exception = System.Exception;
 
 [assembly: CommandClass(typeof(GraphicalElementsCreator))]
 
-namespace AutoCADDev.Graphics
+namespace AutoCADDev.Examples
 {
     /// <summary> 创建各种新的图元对象 </summary>
     internal static class GraphicalElementsCreator
@@ -48,9 +48,11 @@ namespace AutoCADDev.Graphics
                         acPoly.SetDatabaseDefaults();
                         for (int i = 0; i < srcX.Length; i++)
                         {
+                            Point2d pp;
                             acPoly.AddVertexAt(i, new Point2d(srcX[i], srcY[i]), 0, startWidth: 0, endWidth: 0);
                         }
                         acPoly.Closed = close;
+
 
                         // 添加新对象到块表记录和事务中   Add the new object to the block table record and the transaction
                         acBlkTblRec.AppendEntity(acPoly);
@@ -75,51 +77,42 @@ namespace AutoCADDev.Graphics
         [CommandMethod("AddPolyline")]
         public static void AddText(double[] srcX, double[] srcY, bool close)
         {
-            // 获得当前文档和数据库   Get the current document and database
-            Document acDoc = Application.DocumentManager.MdiActiveDocument;
-
-            using (var docLocker = acDoc.LockDocument())
+            using (DocumentModifier docMdf = new DocumentModifier())
             {
-                Database acCurDb = acDoc.Database;
-
-                // 启动一个事务  Start a transaction
-                using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+                try
                 {
-                    try
+                    // 以只读方式打开块表   Open the Block table for read
+                    var acBlkTbl =
+                        docMdf.acTransaction.GetObject(docMdf.acDataBase.BlockTableId, OpenMode.ForRead) as BlockTable;
+
+                    // 以写方式打开模型空间块表记录   Open the Block table record Model space for write
+                    var acBlkTblRec =
+                        docMdf.acTransaction.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as
+                            BlockTableRecord;
+
+                    // 创建一条有两段的多段线   Create a polyline with two segments (3 points)
+                    Polyline acPoly = new Polyline();
+                    acPoly.SetDatabaseDefaults();
+                    for (int i = 0; i < srcX.Length; i++)
                     {
-                        // 以只读方式打开块表   Open the Block table for read
-                        var acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId, OpenMode.ForRead) as BlockTable;
-
-                        // 以写方式打开模型空间块表记录   Open the Block table record Model space for write
-                        var acBlkTblRec =
-                            acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as
-                                BlockTableRecord;
-
-                        // 创建一条有两段的多段线   Create a polyline with two segments (3 points)
-                        Polyline acPoly = new Polyline();
-                        acPoly.SetDatabaseDefaults();
-                        for (int i = 0; i < srcX.Length; i++)
-                        {
-                            acPoly.AddVertexAt(i, new Point2d(srcX[i], srcY[i]), 0, startWidth: 0, endWidth: 0);
-                        }
-                        acPoly.Closed = close;
-
-                        // 添加新对象到块表记录和事务中   Add the new object to the block table record and the transaction
-                        acBlkTblRec.AppendEntity(acPoly);
-
-                        acTrans.AddNewlyCreatedDBObject(acPoly, true);
-
-                        // 保存新对象到数据库中   Save the new object to the database
-                        acTrans.Commit();
+                        acPoly.AddVertexAt(i, new Point2d(srcX[i], srcY[i]), 0, startWidth: 0, endWidth: 0);
                     }
-                    catch (Exception ex)
-                    {
-                        acTrans.Abort(); // Abort the transaction and rollback to the previous state
-                        throw new InvalidOperationException("向文档中添加多段线时出错", ex);
-                    }
-                } // Dispose 事务
-            } // 解锁文档
+                    acPoly.Closed = close;
+
+                    // 添加新对象到块表记录和事务中   Add the new object to the block table record and the transaction
+                    acBlkTblRec.AppendEntity(acPoly);
+
+                    docMdf.acTransaction.AddNewlyCreatedDBObject(acPoly, true);
+
+                    // 保存新对象到数据库中   Save the new object to the database
+                    docMdf.acTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    docMdf.acTransaction.Abort(); // Abort the transaction and rollback to the previous state
+                    throw new InvalidOperationException("向文档中添加多段线时出错", ex);
+                }
+            }
         }
-
     }
 }
